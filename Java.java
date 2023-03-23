524a072c29b45cec0bf3a22ca3503cb8dd5862b1
udashdon4 <90303151+udashdon4@users.noreply.github.com>
2023-03-23 01:51:54 -0400

    Create Java.java

---
+++ Java.java
@@ -0,0 +1,18 @@
+public class Java{
+public static void main(String[] args){
+int[][][] x = {{{1,2},{3,4}},{{5,6},{7,8}}};
+
+int[][][] y = new int[2][2][];
+
+y[0] = x[0].clone();
+
+y[1][1] = x[1][1].clone();
+x[0][0][0] = 100;
+
+x[1][1][1] = 200;
+
+System.out.println(y[0][0][0]+" "+y[1][1][1]);    
+}
+}
+    
+
